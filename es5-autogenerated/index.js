40f02dec35dae3fea6378d7bffd5b5a2984f6ce4
Marc Fabre <nds.kram@gmail.com>
2021-06-23 10:47:49 +0200

    Second publish

--- es5-autogenerated/index.js
+++ es5-autogenerated/index.js
@@ -1,25 +1,9 @@
-'use strict';
+const Server = require('./server');
+const PortFinder = require('portfinder');
 
-var _regenerator = require('babel-runtime/regenerator');
+const PACKAGE_NAME = '[Prerenderer]';
 
-var _regenerator2 = _interopRequireDefault(_regenerator);
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
-
-var Server = require('./server');
-var PortFinder = require('portfinder');
-
-var PACKAGE_NAME = '[Prerenderer]';
-
-var OPTION_SCHEMA = {
+const OPTION_SCHEMA = {
   staticDir: {
     type: String,
     required: true
@@ -33,7 +17,7 @@ var OPTION_SCHEMA = {
 function validateOptionsSchema(schema, options, parent) {
   var errors = [];
 
-  Object.keys(schema).forEach(function (key) {
+  Object.keys(schema).forEach(key => {
     // Required options
     if (schema[key].required && !options[key]) {
       errors.push(`"${parent || ''}${key}" option is required!`);
@@ -47,9 +31,7 @@ function validateOptionsSchema(schema, options, parent) {
     // Array-type options
     if (Array.isArray(schema[key].type) && schema[key].type.indexOf(options[key].constructor) === -1) {
       console.log(schema[key].type.indexOf(options[key].constructor));
-      errors.push(`"${parent || ''}${key}" option must be a ${schema[key].type.map(function (t) {
-        return t.name;
-      }).join(' or ')}!`);
+      errors.push(`"${parent || ''}${key}" option must be a ${schema[key].type.map(t => t.name).join(' or ')}!`);
       // Single-type options.
     } else if (!Array.isArray(schema[key].type) && options[key].constructor !== schema[key].type) {
       errors.push(`"${parent || ''}${key}" option must be a ${schema[key].type.name}!`);
@@ -57,7 +39,7 @@ function validateOptionsSchema(schema, options, parent) {
     }
 
     if (schema[key].children) {
-      errors.push.apply(errors, _toConsumableArray(validateOptionsSchema(schema[key].children, options[key], key)));
+      errors.push(...validateOptionsSchema(schema[key].children, options[key], key));
       return;
     }
   });
@@ -69,10 +51,8 @@ function validateOptionsSchema(schema, options, parent) {
   return errors;
 }
 
-var Prerenderer = function () {
-  function Prerenderer(options) {
-    _classCallCheck(this, Prerenderer);
-
+class Prerenderer {
+  constructor(options) {
     this._options = options || {};
 
     this._server = new Server(this);
@@ -89,112 +69,54 @@ If you are not sure wihch renderer to use, see the documentation at https://gith
 
     if (!this._options.server) this._options.server = {};
 
-    var optionValidationErrors = validateOptionsSchema(OPTION_SCHEMA, this._options);
+    const optionValidationErrors = validateOptionsSchema(OPTION_SCHEMA, this._options);
 
     if (optionValidationErrors.length !== 0) throw new Error(`${PACKAGE_NAME} Options are invalid. Unable to prerender!`);
   }
 
-  _createClass(Prerenderer, [{
-    key: 'initialize',
-    value: function () {
-      var _ref = _asyncToGenerator( /*#__PURE__*/_regenerator2.default.mark(function _callee() {
-        return _regenerator2.default.wrap(function _callee$(_context) {
-          while (1) {
-            switch (_context.prev = _context.next) {
-              case 0:
-                _context.t1 = this._options.server.port;
+  async initialize() {
+    // Initialization is separate from construction because science? (Ideally to initialize the server and renderer separately.)
+    this._options.server.port = this._options.server.port || (await PortFinder.getPortPromise()) || 13010;
+    await this._server.initialize();
+    await this._renderer.initialize();
 
-                if (_context.t1) {
-                  _context.next = 5;
-                  break;
-                }
+    return Promise.resolve();
+  }
 
-                _context.next = 4;
-                return PortFinder.getPortPromise();
+  destroy() {
+    this._renderer.destroy();
+    this._server.destroy();
+  }
 
-              case 4:
-                _context.t1 = _context.sent;
+  getServer() {
+    return this._server;
+  }
 
-              case 5:
-                _context.t0 = _context.t1;
+  getRenderer() {
+    return this._renderer;
+  }
 
-                if (_context.t0) {
-                  _context.next = 8;
-                  break;
-                }
+  getOptions() {
+    return this._options;
+  }
 
-                _context.t0 = 13010;
+  modifyServer(server, stage) {
+    if (this._renderer.modifyServer) this._renderer.modifyServer(this, server, stage);
+  }
 
-              case 8:
-                this._options.server.port = _context.t0;
-                _context.next = 11;
-                return this._server.initialize();
-
-              case 11:
-                _context.next = 13;
-                return this._renderer.initialize();
-
-              case 13:
-                return _context.abrupt('return', Promise.resolve());
-
-              case 14:
-              case 'end':
-                return _context.stop();
-            }
-          }
-        }, _callee, this);
-      }));
-
-      function initialize() {
-        return _ref.apply(this, arguments);
-      }
-
-      return initialize;
-    }()
-  }, {
-    key: 'destroy',
-    value: function destroy() {
-      this._renderer.destroy();
-      this._server.destroy();
-    }
-  }, {
-    key: 'getServer',
-    value: function getServer() {
-      return this._server;
-    }
-  }, {
-    key: 'getRenderer',
-    value: function getRenderer() {
-      return this._renderer;
-    }
-  }, {
-    key: 'getOptions',
-    value: function getOptions() {
-      return this._options;
-    }
-  }, {
-    key: 'modifyServer',
-    value: function modifyServer(server, stage) {
-      if (this._renderer.modifyServer) this._renderer.modifyServer(this, server, stage);
-    }
-  }, {
-    key: 'renderRoutes',
-    value: function renderRoutes(routes) {
-      return this._renderer.renderRoutes(routes, this)
-      // Handle non-ASCII or invalid URL characters in routes by normalizing them back to unicode.
-      // Some browser environments may change unicode or special characters in routes to percent encodings.
-      // We need to convert them back for saving in the filesystem.
-      .then(function (renderedRoutes) {
-        renderedRoutes.forEach(function (rendered) {
-          rendered.route = decodeURIComponent(rendered.route);
-        });
-
-        return renderedRoutes;
+  renderRoutes(routes) {
+    return this._renderer.renderRoutes(routes, this)
+    // Handle non-ASCII or invalid URL characters in routes by normalizing them back to unicode.
+    // Some browser environments may change unicode or special characters in routes to percent encodings.
+    // We need to convert them back for saving in the filesystem.
+    .then(renderedRoutes => {
+      renderedRoutes.forEach(rendered => {
+        rendered.route = decodeURIComponent(rendered.route);
       });
-    }
-  }]);
 
-  return Prerenderer;
-}();
+      return renderedRoutes;
+    });
+  }
+}
 
 module.exports = Prerenderer;
--- es5-autogenerated/server.js
+++ es5-autogenerated/server.js
@@ -1,87 +1,50 @@
-'use strict';
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+const path = require('path');
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var express = require('express');
-var proxy = require('http-proxy-middleware');
-var path = require('path');
-
-var Server = function () {
-  function Server(Prerenderer) {
-    _classCallCheck(this, Server);
-
+class Server {
+  constructor(Prerenderer) {
     this._prerenderer = Prerenderer;
     this._options = Prerenderer.getOptions();
     this._expressServer = express();
     this._nativeServer = null;
   }
 
-  _createClass(Server, [{
-    key: 'initialize',
-    value: function initialize() {
-      var _this = this;
+  initialize() {
+    const server = this._expressServer;
 
-      var server = this._expressServer;
+    this._prerenderer.modifyServer(this, 'pre-static');
 
-      this._prerenderer.modifyServer(this, 'pre-static');
+    server.get('*.*', express.static(this._options.staticDir, {
+      dotfiles: 'allow'
+    }));
 
-      server.get('*.*', express.static(this._options.staticDir, {
-        dotfiles: 'allow'
-      }));
+    this._prerenderer.modifyServer(this, 'post-static');
 
-      this._prerenderer.modifyServer(this, 'post-static');
+    this._prerenderer.modifyServer(this, 'pre-fallback');
 
-      this._prerenderer.modifyServer(this, 'pre-fallback');
-
-      if (this._options.server && this._options.server.proxy) {
-        var _iteratorNormalCompletion = true;
-        var _didIteratorError = false;
-        var _iteratorError = undefined;
-
-        try {
-          for (var _iterator = Object.keys(this._options.server.proxy)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-            var proxyPath = _step.value;
-
-            server.use(proxyPath, proxy(this._options.server.proxy[proxyPath]));
-          }
-        } catch (err) {
-          _didIteratorError = true;
-          _iteratorError = err;
-        } finally {
-          try {
-            if (!_iteratorNormalCompletion && _iterator.return) {
-              _iterator.return();
-            }
-          } finally {
-            if (_didIteratorError) {
-              throw _iteratorError;
-            }
-          }
-        }
+    if (this._options.server && this._options.server.proxy) {
+      for (let proxyPath of Object.keys(this._options.server.proxy)) {
+        server.use(proxyPath, proxy(this._options.server.proxy[proxyPath]));
       }
-
-      server.get('*', function (req, res) {
-        res.sendFile(_this._options.indexPath ? _this._options.indexPath : path.join(_this._options.staticDir, 'index.html'));
-      });
-
-      this._prerenderer.modifyServer(this, 'post-fallback');
-
-      return new Promise(function (resolve, reject) {
-        _this._nativeServer = server.listen(_this._options.server.port, function () {
-          resolve();
-        });
-      });
     }
-  }, {
-    key: 'destroy',
-    value: function destroy() {
-      this._nativeServer.close();
-    }
-  }]);
 
-  return Server;
-}();
+    server.get('*', (req, res) => {
+      res.sendFile(this._options.indexPath ? this._options.indexPath : path.join(this._options.staticDir, 'index.html'));
+    });
+
+    this._prerenderer.modifyServer(this, 'post-fallback');
+
+    return new Promise((resolve, reject) => {
+      this._nativeServer = server.listen(this._options.server.port, () => {
+        resolve();
+      });
+    });
+  }
+
+  destroy() {
+    this._nativeServer.close();
+  }
+}
 
 module.exports = Server;
